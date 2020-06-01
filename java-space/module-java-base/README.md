d7691c5a35398cbd28adaff23b2a7c528295ffe1
bzmxl <bzmxl@outlook.com>
2020-06-01 22:47:20 +0800

    add String

--- java-space/module-java-base/README.md
+++ java-space/module-java-base/README.md
@@ -1,4 +1,6 @@
 ```text
+https://docs.oracle.com/en/java/javase/14/docs/api/java.base/module-summary.html
+===
 StringBuffer
 StringBuilder
 ```
---
+++ java-space/module-java-base/src/main/java/bzmxl/lang/StringTest.java
@@ -0,0 +1,8 @@
+package bzmxl.lang;
+
+public class StringTest {
+    public static void main(String[] args) {
+        //public native String intern()
+        String s = new String();
+    }
+}
